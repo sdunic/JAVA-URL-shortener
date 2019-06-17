c4eb2b5262baa766866b9f47804cebf92f43d54b
Stefan Dunić <sdunic@gmail.com>
2019-06-17 12:47:10 +0200

    redirect url test

--- src/main/java/com/sd/urlshortener/UrlShortener.java
+++ src/main/java/com/sd/urlshortener/UrlShortener.java
@@ -1,9 +1,14 @@
 package com.sd.urlshortener;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RequestMapping("/")
 @RestController
@@ -12,11 +17,19 @@ public class UrlShortener {
     int counter = 0;
 
     @GetMapping("/{id}")
-    public Integer getUrl(@PathVariable String id) {
-        System.out.println("Shortened URL: " + id);
-        
-        counter++;
+    public RedirectView redirectToLongUrl(@PathVariable String id, HttpServletRequest req, HttpServletResponse res) {
+        String longUrl = "/";
+        RedirectView redirectView = new RedirectView();
 
-        return counter;
+        if(id.equals("stefan")) {
+            longUrl = "https://stefandunic.com";
+            redirectView.setStatusCode(HttpStatus.PERMANENT_REDIRECT);
+        }
+        else {
+            redirectView.setStatusCode(HttpStatus.NOT_FOUND);
+        }
+        redirectView.setUrl(longUrl);
+
+        return redirectView;
     }
 }

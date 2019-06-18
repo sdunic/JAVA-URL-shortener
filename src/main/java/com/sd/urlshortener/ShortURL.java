4f3bae1c1d7ee53b6c8dea6e566d3c6d647dfb55
Stefan Dunić <sdunic@gmail.com>
2019-06-18 19:13:23 +0200

    encode62 and shortUrl added to shortURL class

--- src/main/java/com/sd/urlshortener/ShortURL.java
+++ src/main/java/com/sd/urlshortener/ShortURL.java
@@ -4,13 +4,26 @@ public class ShortURL {
 
     public String redirectType = "302";
     String longUrl = "";
+    String shortUrl = "";
     String accountId = "";
 
-    public ShortURL(String longUrl) {
+    public ShortURL(String longUrl, Integer i) {
         this.longUrl = longUrl;
+        this.shortUrl = base62_encode(i);
     }
 
     public String toString() { 
-        return "redirectType: '" + this.redirectType + "', longUrl: '" + this.longUrl + "', accountId: '" + this.accountId + "'";
+        return "redirectType: '" + this.redirectType + "', shortUrl: '" + this.shortUrl + "', longUrl: '" + this.longUrl + "', accountId: '" + this.accountId + "'";
     } 
+
+    public String base62_encode(Integer i){
+        String[]  s = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+        String output = "";
+        while (i > 0) {
+            output = s[i % 62] + output;
+            i = i/62;
+        }
+        return output;
+    }
 }  
--- src/main/java/com/sd/urlshortener/UrlShortener.java
+++ src/main/java/com/sd/urlshortener/UrlShortener.java
@@ -24,38 +24,25 @@ public class UrlShortener {
 
     HashMap<String, ShortURL> urls = new HashMap<String, ShortURL>();
 
-    public String base62_encode(Integer i){
-        String[]  s = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-
-        String output = "";
-        while (i > 0) {
-            output = s[i % 62] + output;
-            i = i/62;
-        }
-        return output;
-    }
-
     @PostMapping("/register")
     public JSONObject createShortUrl(@RequestBody String obj, HttpServletRequest request) throws ParseException {
         JSONObject output = new JSONObject();
 
-        String shortUrl = base62_encode(++counter);
-
         JSONParser parser = new JSONParser();
         JSONObject json = (JSONObject) parser.parse(obj);
 
-        ShortURL s = new ShortURL(json.get("url").toString());
+        ShortURL s = new ShortURL(json.get("url").toString(), ++counter);
 
         if(json.get("redirectType") != null) {
             s.redirectType = json.get("redirectType").toString();
         }
         
-        urls.put(shortUrl, s);
+        urls.put(s.shortUrl, s);
 
         System.out.println(json);
         System.out.println(urls);
         
-        String outputUrl = request.getRequestURL().subSequence(0, request.getRequestURL().length() - 8) + shortUrl;
+        String outputUrl = request.getRequestURL().subSequence(0, request.getRequestURL().length() - 8) + s.shortUrl;
         output.put("shortUrl", outputUrl);
         System.out.print(output);
 

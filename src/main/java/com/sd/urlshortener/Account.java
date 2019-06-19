83b3e28a41e8efeb23962c092bc7040ca2a39863
Stefan Dunić <sdunic@gmail.com>
2019-06-19 18:44:26 +0200

    account, account registration, random password, api basic authorization, respones statuses

--- src/main/java/com/sd/urlshortener/Account.java
+++ src/main/java/com/sd/urlshortener/Account.java
@@ -1,22 +1,53 @@
 package com.sd.urlshortener;
 
+import java.security.SecureRandom;
+import java.util.Base64;
 import java.util.HashMap;
 
 public class Account {
 
+    String accountId;
     String password;
-    public HashMap<String, Integer> urls = new HashMap<String, Integer>();
+    public HashMap<String, Integer> urlStatistics = new HashMap<String, Integer>();
 
-
-    public Account() {
+    public Account(String accountId) {
+        this.accountId = accountId;
         this.password = generateRandomPassword();
     }
 
     public String generateRandomPassword(){
-        return "";
+        final String characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        SecureRandom random = new SecureRandom();
+        
+        String result="";
+        for(int i = 0; i<8; i++){
+            int index = random.nextInt(characters.length());
+            result += characters.charAt(index);
+        }
+
+        return result;
+    }
+
+    public void addUrl(String longUrl) {
+        urlStatistics.put(longUrl, 0);
+    }
+
+    public void addVisit(String longUrl) {
+        urlStatistics.put(longUrl, urlStatistics.get(longUrl) + 1);
+    }
+
+    public String getStatistics() {
+        return this.urlStatistics.toString();
+    }
+
+    public String getAuthorization() {
+        String originalInput = accountId + ":" + password;
+        String encodedString = Base64.getEncoder().encodeToString(originalInput.getBytes());
+        
+        return encodedString;
     }
 
     public String toString() { 
-        return "";
+        return "accountId: " + this.accountId + ", password: " + this.password + ", statistics: " + this.urlStatistics;
     } 
 }  
--- src/main/java/com/sd/urlshortener/UrlShortener.java
+++ src/main/java/com/sd/urlshortener/UrlShortener.java
@@ -3,15 +3,15 @@ package com.sd.urlshortener;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -23,36 +23,81 @@ public class UrlShortener {
     int counter = 0;
 
     HashMap<String, ShortURL> urls = new HashMap<String, ShortURL>();
+    HashMap<String, Account> accounts = new HashMap<String, Account>();
+    HashMap<String, String> authorizationCodes = new HashMap<String, String>();
 
 
-    @PostMapping("/register")
-    public JSONObject createShortUrl(@RequestBody String obj, HttpServletRequest request) throws ParseException {
+    @PostMapping("/account")
+    public JSONObject createAccount(@RequestBody JSONObject json, HttpServletResponse response) {
         JSONObject output = new JSONObject();
 
-        JSONParser parser = new JSONParser();
-        JSONObject json = (JSONObject) parser.parse(obj);
+        if(accounts.get(json.get("AccountId")) == null)
+        {
+            Account account = new Account(json.get("AccountId").toString());
+            accounts.put(account.accountId, account);
+            authorizationCodes.put(account.getAuthorization(), account.accountId);
 
-        ShortURL s = new ShortURL(json.get("url").toString(), ++counter);
-        if(json.get("redirectType") != null) {
-            s.redirectType = json.get("redirectType").toString();
+            output.put("success", true);
+            output.put("description", "Your account is opened");
+            output.put("password", account.password);
+            response.setStatus(200);
+        } 
+        else 
+        {
+            output.put("success", false);
+            output.put("description", "Account already exists");
+            response.setStatus(200);  
         }
-        urls.put(s.shortUrl, s);
-
-        System.out.println(urls);
         
-        String outputUrl = request.getRequestURL().subSequence(0, request.getRequestURL().length() - 8) + s.shortUrl;
-        output.put("shortUrl", outputUrl);
-
+        System.out.println(accounts);
         return output;
+    }
+
+    @PostMapping("/register")
+    public JSONObject createShortUrl(@RequestHeader String Authorization, @RequestBody JSONObject json, HttpServletRequest request, HttpServletResponse response) {
+        JSONObject output = new JSONObject();
+
+        //u slučaju uspješen registracije trebamo vratiti status 200, a u protivnom vraćamo 401
+
+        String authorizationCode = Authorization.split(" ")[1];
+        String accountId = authorizationCodes.get(authorizationCode);
+        Account account = accounts.get(accountId);
+
+        if(account != null) {
+            String longUrl = json.get("url").toString();
+
+            ShortURL s = new ShortURL(longUrl, accountId, ++counter);
+            account.addUrl(longUrl);
+
+            if(json.get("redirectType") != null) {
+                s.redirectType = json.get("redirectType").toString();
+            }
+            urls.put(s.shortUrl, s);
+            
+            String outputUrl = request.getRequestURL().subSequence(0, request.getRequestURL().length() - 8) + s.shortUrl;
+            output.put("shortUrl", outputUrl);
+            
+            System.out.println(urls);
+            response.setStatus(200);
+            return output;
+        }
+        else {
+            response.setStatus(401);
+            return output;
+        }
     }
 
     @GetMapping("/{id}")
     public RedirectView redirectToLongUrl(@PathVariable String id) {
         RedirectView redirectView = new RedirectView();
         String longUrl = "/";
+        String accountId;
         ShortURL s = urls.get(id);
         if(s != null) {
             longUrl = s.longUrl;
+            accountId = s.accountId;
+            accounts.get(accountId).addVisit(longUrl);
+
             if(s.redirectType.equals("301"))
                 redirectView.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
             else

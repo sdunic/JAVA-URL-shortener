5a102660897efd1658a06375ee56f4b52c4b4e1b
Stefan Dunić <sdunic@gmail.com>
2019-06-22 17:52:24 +0200

    added more content to help page, code cleanup

--- src/main/java/com/sd/urlshortener/HelpController.java
+++ src/main/java/com/sd/urlshortener/HelpController.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelpController {
@@ -15,6 +14,5 @@ public class HelpController {
         String baseUrl = request.getRequestURL().substring(0, request.getRequestURL().length() - 4);
         model.addAttribute("baseUrl", baseUrl);
         return "helpPage";
-    }
-    
+    }    
 }
